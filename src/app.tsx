03b8d12197fcd9ba94fe7021e40f55ed449a5140
Corgi <dmprogramhh@mail.ru>
2023-01-26 23:22:30 +0300

    add-imageLinkForm

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/logo";
-
+import ImageLinkForm from "./components/imageLinkForm/imageLinkForm";
 const App = () => {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
-        <FaceRecognition /> */}
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 };
