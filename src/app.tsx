9490da4720dbd7e12b03ca8d892b3012ed2dc6ca
Corgi <dmprogramhh@mail.ru>
2023-02-02 13:28:02 +0300

    add-particles

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/logo";
 import ImageLinkForm from "./components/imageLinkForm/imageLinkForm";
+import Rank from "./components/rank/rank";
+import Particle from "./components/particles/particle";
+import "./app.css";
+
 const App = () => {
   return (
     <div className="App">
+      <Particle />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
---
+++ src/components/rank/rank.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./rank.css";
+const Rank = () => {
+  return (
+    <div className="content">
+      <div className="white f3">{"Dmitriy, your current rank is..."}</div>
+      <div className="white f1">{"#5"}</div>
+    </div>
+  );
+};
+export default Rank;
